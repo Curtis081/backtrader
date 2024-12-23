6c6c69f2003e03f80c2371b6071f9fbaa61425d3
Curtis081 <ar06053127hg@gmail.com>
2024-12-23 19:22:07 +0800

    add vix th strategy back test params

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from back_trader.strategy.vix import vixCross
 from simulation_setting import initial_cash, commission, start_date, end_date
 
 
-def backtrader_with_strategy(data_feed, strategy, strategy_params=None):
+def backtrader_with_strategy(data_feed, strategy, cerebro_plot=True, strategy_params=None):
     cerebro = bt.Cerebro()
     cerebro.adddata(data_feed)
 
@@ -52,7 +52,8 @@ def backtrader_with_strategy(data_feed, strategy, strategy_params=None):
     total_return = (final_value - initial_cash) / initial_cash * 100
     print(total_return)
 
-    cerebro.plot()
+    if cerebro_plot:
+        cerebro.plot()
 
     return total_return
 
@@ -85,32 +86,56 @@ def write_dict_to_file(file_path, data, header=None):
             file.write(header + "\n")
             file.write("-" * len(header) + "\n")
 
-        file.write(f"\n")
-        for key, value in data.items():
-            file.write(f"{key}: {value}\n")
+        if isinstance(data, dict):
+            file.write(f"\n")
+            for key, value in data.items():
+                file.write(f"{key}: {value}\n")
+        elif isinstance(data, str):
+            file.write(f"{data}\n")
 
     print(f"dirctory contents written to {file_path}")
+
+
+def best_params(buy_and_hold_total_return):
+    file_path = "best_params.txt"
+    file_path2 = "greater_than_buy_and_hold_params.txt"
+    delete_file_if_exists(file_path)
+    delete_file_if_exists(file_path2)
+    best_params = {'rolling_days': 1, 'vix_th': 1, 'total_return': 0}
+    write_dict_to_file(file_path, 'buy_and_hold_total_return = ' + str(buy_and_hold_total_return), header="")
+    write_dict_to_file(file_path2, 'buy_and_hold_total_return = ' + str(buy_and_hold_total_return), header="")
+    counter = 0
+    for rolling_days, vix_th in itertools.product(range(1, 2), range(1, 101, 1)):
+        counter += 1
+        params = {'rolling_days': rolling_days, 'vix_th': vix_th}
+        total_return = backtrader_with_strategy(data_feed, vixCross, cerebro_plot=False, strategy_params=params)
+        if total_return > best_params['total_return']:
+            best_params = {'rolling_days': rolling_days, 'vix_th': vix_th, 'total_return': total_return}
+            print(best_params)
+            write_dict_to_file(file_path, best_params, header="")
+        if total_return > buy_and_hold_total_return:
+            better_than_buy_and_hold_params = {'rolling_days': rolling_days, 'vix_th': vix_th,
+                                               'total_return': total_return}
+            print(f'better_than_buy_and_hold_params')
+            write_dict_to_file(file_path2, better_than_buy_and_hold_params, header="")
+
+    print(counter)
+    print(best_params)
 
 
 if __name__ == '__main__':
     ticker = 'SPY'
     data_feed = get_data_from_yahoo(ticker, start_date, end_date)
 
-    backtrader_with_strategy(data_feed, BuyAndHold)
+    buy_and_hold_total_return = backtrader_with_strategy(data_feed, BuyAndHold)
 
-    params = {'rolling_days': 1, 'vix_th': 44}
+    # 通常VIX指數超過40時，表示市場對未來的非理性恐慌
+    # params = {'rolling_days': 1, 'vix_th': 44}  # start_date = '1993-01-29' # SPY stock IPO
+    # params = {'rolling_days': 1, 'vix_th': 44}  # start_date = '1998-01-01' # 2000 stock market crash
+    # params = {'rolling_days': 1, 'vix_th': 44}  # start_date = '2004-03-29' # 2008 stock market crash
+    params = {'rolling_days': 1, 'vix_th': 51}  # start_date = '2010-10-09' # VOO stock for compare
+    # params = {'rolling_days': 1, 'vix_th': 51}  # start_date = '2019-01-01' # 2020 stock market crash
+    # params = {'rolling_days': 1, 'vix_th': 34}  # start_date = '2021-01-01' # 2022 stock market crash # not best choice
     backtrader_with_strategy(data_feed, vixCross, strategy_params=params)
 
-    # file_path = "best_params.txt"
-    # delete_file_if_exists(file_path)
-    # best_params = {'rolling_days': 1, 'vix_th': 1, 'total_return': 0}
-    # for rolling_days, vix_th in itertools.product(range(1, 2), range(1, 100, 1)):
-    #     params = {'rolling_days': rolling_days, 'vix_th': vix_th}
-    #     total_return = backtrader_with_strategy(data_feed, vixCross, strategy_params=params)
-    #     if total_return > best_params['total_return']:
-    #         best_params['rolling_days'] = rolling_days
-    #         best_params['vix_th'] = vix_th
-    #         best_params['total_return'] = total_return
-    #         print(f'rolling_days: {rolling_days}, vix_th: {vix_th}, total_return: {total_return}')
-    #         write_dict_to_file(file_path, best_params, header="")
-    # print(best_params)
+    best_params(buy_and_hold_total_return)
--- simulation_setting.py
+++ simulation_setting.py
@@ -1,7 +1,7 @@
 initial_cash = 10000
 commission = 0.003
 
-start_date = '1993-02-01'
-end_date = '2024-12-01'
+start_date = '2010-10-09'
+end_date = '2024-12-21'
 
 do_log_print = False
